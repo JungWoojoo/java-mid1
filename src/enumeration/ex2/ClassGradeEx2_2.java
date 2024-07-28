a86754d68ddff7a661b2b4da0422255bd056d82d
Woojoo JUNG <jwj5045@gmail.com>
2024-07-28 15:57:37 +0900

    제 5강 열거형 Enum

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,15 @@
+package enumeration.ex2;
+
+import enumeration.ex3.Grade;
+
+public class ClassGradeEx2_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        //ClassGrade newClassGrade = new ClassGrade();
+        //int result = discountService.discount(newClassGrade, price);
+        //System.out.println("newClassGrade의 할인 금액 = " + result);
+    }
+}
